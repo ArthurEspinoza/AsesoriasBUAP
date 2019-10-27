f149e3710e55a97c02bc75d854f1b57d1bb2c391
ArthurEspinoza <arturoeq64@gmail.com>
2019-10-26 21:35:12 -0500

    Se cambio de mysqli a pdo en el de guardarDatos

--- guardarDatos.php
+++ guardarDatos.php
@@ -1,53 +1,113 @@
 <?php 
+    header("Content-type:text/html; charset=utf-8");
     session_start();
+    include('php/config.php');
+    $conn = getDB();
     $idProfe = $_SESSION['noTrabajador'];
     echo $idProfe;
-/*for ($i=1; $i<=12; $i++) { 
-    for ($j=1; $j<=5; $j++) { 
-        ${$i.$j} = $_POST[$i.$j];
-        echo "campo".$i.$j.":".${$i.$j};
-        echo "<br>";
-    }
-}*/
-
-    $mysqli = new mysqli('localhost', 'root', '', 'asesoria');
-
-    if ($mysqli->connect_error) {
-        die('Connect Error (' . $mysqli->connect_errno . ') '
-                . $mysqli->connect_error);
-    }
-
-    if (mysqli_connect_error()) {
-        die('Connect Error (' . mysqli_connect_errno() . ') '
-                . mysqli_connect_error());
-    }
     for ($i=1; $i<=12; $i++) { 
-        for ($j=1; $j<=5; $j++) { 
+        for ($j=1; $j<=5; $j++) {
+            //echo $_POST[$i.$j];
             $GLOBALS[$i.$j] = $_POST[$i.$j];
             //echo "campo".$i.$j.":".$GLOBALS[$i.$j];
             //echo "<br>";
         }
     }
-    $existe = $mysqli->query( "SELECT * FROM lunes WHERE noTrabajador='$idProfe'");
-    if( $resultado = $existe->fetch_assoc()!=null){
-        //echo "Entre al if";
-        $busqueda1=$mysqli->query("UPDATE lunes SET siete='$GLOBALS[11]',ocho='$GLOBALS[21]',nueve='$GLOBALS[31]',diez='$GLOBALS[41]',once='$GLOBALS[51]',doce='$GLOBALS[61]',trece='$GLOBALS[71]',catorce='$GLOBALS[81]',quince='$GLOBALS[91]',diezseis='$GLOBALS[101]',diezsiete='$GLOBALS[111]',diezocho='$GLOBALS[121]' WHERE noTrabajador='$idProfe'");
-        $busqueda2=$mysqli->query("UPDATE martes SET siete='$GLOBALS[12]',ocho='$GLOBALS[22]',nueve='$GLOBALS[32]',diez='$GLOBALS[42]',once='$GLOBALS[52]',doce='$GLOBALS[62]',trece='$GLOBALS[72]',catorce='$GLOBALS[82]',quince='$GLOBALS[92]',diezseis='$GLOBALS[102]',diezsiete='$GLOBALS[112]',diezocho='$GLOBALS[122]' WHERE noTrabajador=$idProfe");
-        $busqueda3=$mysqli->query("UPDATE miercoles SET siete='$GLOBALS[13]',ocho='$GLOBALS[23]',nueve='$GLOBALS[33]',diez='$GLOBALS[43]',once='$GLOBALS[53]',doce='$GLOBALS[63]',trece='$GLOBALS[73]',catorce='$GLOBALS[83]',quince='$GLOBALS[93]',diezseis='$GLOBALS[103]',diezsiete='$GLOBALS[113]',diezocho='$GLOBALS[123]' WHERE noTrabajador=$idProfe");
-        $busqueda4=$mysqli->query("UPDATE jueves SET siete='$GLOBALS[14]',ocho='$GLOBALS[24]',nueve='$GLOBALS[34]',diez='$GLOBALS[44]',once='$GLOBALS[54]',doce='$GLOBALS[64]',trece='$GLOBALS[74]',catorce='$GLOBALS[84]',quince='$GLOBALS[94]',diezseis='$GLOBALS[104]',diezsiete='$GLOBALS[114]',diezocho='$GLOBALS[124]' WHERE noTrabajador=$idProfe");
-        $busqueda5=$mysqli->query("UPDATE viernes SET siete='$GLOBALS[15]',ocho='$GLOBALS[25]',nueve='$GLOBALS[35]',diez='$GLOBALS[45]',once='$GLOBALS[55]',doce='$GLOBALS[65]',trece='$GLOBALS[75]',catorce='$GLOBALS[85]',quince='$GLOBALS[95]',diezseis='$GLOBALS[105]',diezsiete='$GLOBALS[115]',diezocho='$GLOBALS[125]' WHERE noTrabajador=$idProfe");
+    $datosLunes = [
+        'siete' => $GLOBALS[11],
+        'ocho' => $GLOBALS[21],
+        'nueve' => $GLOBALS[31],
+        'diez' => $GLOBALS[41],
+        'once' => $GLOBALS[51],
+        'doce' => $GLOBALS[61],
+        'trece' => $GLOBALS[71],
+        'catorce' => $GLOBALS[81],
+        'quince' => $GLOBALS[91],
+        'diezseis' => $GLOBALS[101],
+        'diezsiete' => $GLOBALS[111],
+        'diezocho' => $GLOBALS[121],
+    ];
+    $datosMartes = [
+        'siete' => $GLOBALS[12],
+        'ocho' => $GLOBALS[22],
+        'nueve' => $GLOBALS[32],
+        'diez' => $GLOBALS[42],
+        'once' => $GLOBALS[52],
+        'doce' => $GLOBALS[62],
+        'trece' => $GLOBALS[72],
+        'catorce' => $GLOBALS[82],
+        'quince' => $GLOBALS[92],
+        'diezseis' => $GLOBALS[102],
+        'diezsiete' => $GLOBALS[112],
+        'diezocho' => $GLOBALS[122],
+    ];
+    $datosMiercoles = [
+        'siete' => $GLOBALS[13],
+        'ocho' => $GLOBALS[23],
+        'nueve' => $GLOBALS[33],
+        'diez' => $GLOBALS[43],
+        'once' => $GLOBALS[53],
+        'doce' => $GLOBALS[63],
+        'trece' => $GLOBALS[73],
+        'catorce' => $GLOBALS[83],
+        'quince' => $GLOBALS[93],
+        'diezseis' => $GLOBALS[103],
+        'diezsiete' => $GLOBALS[113],
+        'diezocho' => $GLOBALS[123],
+    ];
+    $datosJueves = [
+        'siete' => $GLOBALS[14],
+        'ocho' => $GLOBALS[24],
+        'nueve' => $GLOBALS[34],
+        'diez' => $GLOBALS[44],
+        'once' => $GLOBALS[54],
+        'doce' => $GLOBALS[64],
+        'trece' => $GLOBALS[74],
+        'catorce' => $GLOBALS[84],
+        'quince' => $GLOBALS[94],
+        'diezseis' => $GLOBALS[104],
+        'diezsiete' => $GLOBALS[114],
+        'diezocho' => $GLOBALS[124],
+    ];
+    $datosViernes = [
+        'siete' => $GLOBALS[15],
+        'ocho' => $GLOBALS[25],
+        'nueve' => $GLOBALS[35],
+        'diez' => $GLOBALS[45],
+        'once' => $GLOBALS[55],
+        'doce' => $GLOBALS[65],
+        'trece' => $GLOBALS[75],
+        'catorce' => $GLOBALS[85],
+        'quince' => $GLOBALS[95],
+        'diezseis' => $GLOBALS[105],
+        'diezsiete' => $GLOBALS[115],
+        'diezocho' => $GLOBALS[125],
+    ];
+    $profe = $conn->prepare('SELECT * FROM lunes WHERE noTrabajador=:prof');
+    $profe->bindParam(':prof', $idProfe, PDO::PARAM_INT);
+    $profe->execute();
+    if ($profe->rowCount()>0) {
+        $sql1 = $conn->prepare('UPDATE lunes SET siete=:siete, ocho=:ocho, nueve=:nueve, diez=:diez, once=:once, doce=:doce, trece=:trece, catorce=:catorce, quince=:quince, diezseis=:diezseis, diezsiete=:diezsiete, diezocho=:diezocho');
+        $sql2 = $conn->prepare('UPDATE martes SET siete=:siete, ocho=:ocho, nueve=:nueve, diez=:diez, once=:once, doce=:doce, trece=:trece, catorce=:catorce, quince=:quince, diezseis=:diezseis, diezsiete=:diezsiete, diezocho=:diezocho');
+        $sql3 = $conn->prepare('UPDATE miercoles SET siete=:siete, ocho=:ocho, nueve=:nueve, diez=:diez, once=:once, doce=:doce, trece=:trece, catorce=:catorce, quince=:quince, diezseis=:diezseis, diezsiete=:diezsiete, diezocho=:diezocho');
+        $sql4 = $conn->prepare('UPDATE jueves SET siete=:siete, ocho=:ocho, nueve=:nueve, diez=:diez, once=:once, doce=:doce, trece=:trece, catorce=:catorce, quince=:quince, diezseis=:diezseis, diezsiete=:diezsiete, diezocho=:diezocho');
+        $sql5 = $conn->prepare('UPDATE viernes SET siete=:siete, ocho=:ocho, nueve=:nueve, diez=:diez, once=:once, doce=:doce, trece=:trece, catorce=:catorce, quince=:quince, diezseis=:diezseis, diezsiete=:diezsiete, diezocho=:diezocho');
+        $sql1->execute($datosLunes);
+        $sql2->execute($datosMartes);
+        $sql3->execute($datosMiercoles);
+        $sql4->execute($datosJueves);
+        $sql5->execute($datosViernes);
     }else{
-        //echo "entre al else";
-        $busqueda1=$mysqli->query("INSERT INTO lunes(noTrabajador,siete,ocho,nueve,diez,once,doce,trece,catorce,quince,diezseis,diezsiete,diezocho) 
-        VALUES ('$idProfe','$GLOBALS[11]','$GLOBALS[21]','$GLOBALS[31]','$GLOBALS[41]','$GLOBALS[51]','$GLOBALS[61]','$GLOBALS[71]','$GLOBALS[81]','$GLOBALS[91]','$GLOBALS[101]','$GLOBALS[111]','$GLOBALS[121]')");
-        $busqueda2=$mysqli->query("INSERT INTO martes(noTrabajador,siete,ocho,nueve,diez,once,doce,trece,catorce,quince,diezseis,diezsiete,diezocho) 
-        VALUES ('$idProfe','$GLOBALS[12]','$GLOBALS[22]','$GLOBALS[32]','$GLOBALS[42]','$GLOBALS[52]','$GLOBALS[62]','$GLOBALS[72]','$GLOBALS[82]','$GLOBALS[92]','$GLOBALS[102]','$GLOBALS[112]','$GLOBALS[122]')");
-        $busqueda3=$mysqli->query("INSERT INTO miercoles(noTrabajador,siete,ocho,nueve,diez,once,doce,trece,catorce,quince,diezseis,diezsiete,diezocho) 
-        VALUES ('$idProfe','$GLOBALS[13]','$GLOBALS[23]','$GLOBALS[33]','$GLOBALS[43]','$GLOBALS[53]','$GLOBALS[63]','$GLOBALS[73]','$GLOBALS[83]','$GLOBALS[93]','$GLOBALS[103]','$GLOBALS[113]','$GLOBALS[123]')");
-        $busqueda4=$mysqli->query("INSERT INTO jueves(noTrabajador,siete,ocho,nueve,diez,once,doce,trece,catorce,quince,diezseis,diezsiete,diezocho) 
-        VALUES ('$idProfe','$GLOBALS[14]','$GLOBALS[24]','$GLOBALS[34]','$GLOBALS[44]','$GLOBALS[54]','$GLOBALS[64]','$GLOBALS[74]','$GLOBALS[84]','$GLOBALS[94]','$GLOBALS[104]','$GLOBALS[114]','$GLOBALS[124]')");
-        $busqueda5=$mysqli->query("INSERT INTO viernes(noTrabajador,siete,ocho,nueve,diez,once,doce,trece,catorce,quince,diezseis,diezsiete,diezocho) 
-        VALUES ('$idProfe','$GLOBALS[15]','$GLOBALS[25]','$GLOBALS[35]','$GLOBALS[45]','$GLOBALS[55]','$GLOBALS[65]','$GLOBALS[75]','$GLOBALS[85]','$GLOBALS[95]','$GLOBALS[105]','$GLOBALS[115]','$GLOBALS[125]')");
+        $sql1 = $conn->prepare('INSERT INTO lunes(siete, ocho, nueve, diez, once, doce, trece, catorce, quince, diezseis, diezsiete, diezocho) VALUES(:siete, :ocho, :nueve, :diez, :once, :doce, :trece, :catorce, :quince, :diezseis, :diezsiete, :diezocho)');
+        $sql2 = $conn->prepare('INSERT INTO martes(siete, ocho, nueve, diez, once, doce, trece, catorce, quince, diezseis, diezsiete, diezocho) VALUES(:siete, :ocho, :nueve, :diez, :once, :doce, :trece, :catorce, :quince, :diezseis, :diezsiete, :diezocho)');
+        $sql3 = $conn->prepare('INSERT INTO miercoles(siete, ocho, nueve, diez, once, doce, trece, catorce, quince, diezseis, diezsiete, diezocho) VALUES(:siete, :ocho, :nueve, :diez, :once, :doce, :trece, :catorce, :quince, :diezseis, :diezsiete, :diezocho)');
+        $sql4 = $conn->prepare('INSERT INTO jueves(siete, ocho, nueve, diez, once, doce, trece, catorce, quince, diezseis, diezsiete, diezocho) VALUES(:siete, :ocho, :nueve, :diez, :once, :doce, :trece, :catorce, :quince, :diezseis, :diezsiete, :diezocho)');
+        $sql5 = $conn->prepare('INSERT INTO viernes(siete, ocho, nueve, diez, once, doce, trece, catorce, quince, diezseis, diezsiete, diezocho) VALUES(:siete, :ocho, :nueve, :diez, :once, :doce, :trece, :catorce, :quince, :diezseis, :diezsiete, :diezocho)');
+        $sql1->execute($datosLunes);
+        $sql2->execute($datosMartes);
+        $sql3->execute($datosMiercoles);
+        $sql4->execute($datosJueves);
+        $sql5->execute($datosViernes);
     }
     echo '<script>
         window.location.href="horario.php";
--- php/verHorario.php
+++ php/verHorario.php
@@ -1,10 +1,45 @@
 <?php
 header("Content-type:text/html; charset=utf-8");
-//include('config.php');
-//$conn = getDB();
+include('config.php');
+$conn = getDB();
 //$conn->exec("set names 'utf8'");
 $idProfe = $_GET['profe'];
-//echo $idProf;
+$datosLunes;
+$datosMartes;
+$datosMiercoles;
+$datosJueves;
+$datosViernes;
+//Nombre profe;
+$profe = $conn->prepare('SELECT * FROM Profesor where noTrabajador=:nT');
+$profe->bindParam(':nT', $idProfe, PDO::PARAM_INT);
+$profe->execute();
+$datosP = $profe->fetch(PDO::FETCH_ASSOC);
+//HorarioProfe
+$horarioLunes = $conn->prepare('SELECT * FROM lunes where noTrabajador=:nT');
+$horarioMartes = $conn->prepare('SELECT * FROM martes where noTrabajador=:nT');
+$horarioMiercoles = $conn->prepare('SELECT * FROM miercoles where noTrabajador=:nT'); 
+$horarioJueves = $conn->prepare('SELECT * FROM jueves where noTrabajador=:nT');
+$horarioViernes = $conn->prepare('SELECT * FROM viernes where noTrabajador=:nT');
+$horarioLunes->bindParam(':nT', $idProfe, PDO::PARAM_INT);
+$horarioMartes->bindParam(':nT', $idProfe, PDO::PARAM_INT);
+$horarioMiercoles->bindParam(':nT', $idProfe, PDO::PARAM_INT);
+$horarioJueves->bindParam(':nT', $idProfe, PDO::PARAM_INT);
+$horarioViernes->bindParam(':nT', $idProfe, PDO::PARAM_INT);
+if ($horarioLunes->execute()) {
+    $datosLunes = $horarioLunes->fetchAll();
+}else{echo"No hay lunes";}
+if($horarioMartes->execute()){
+    $datosMartes = $horarioMartes->fetchAll();
+}else{echo"No hay martes";}
+if ($horarioMiercoles->execute()) {
+    $datosMiercoles = $horarioMiercoles->fetchAll();
+}else{echo "No hay miercoles";}
+if ($horarioJueves->execute()) {
+    $datosJueves = $horarioJueves->fetchAll();
+}else{echo"No hay jueves";}
+if ($horarioViernes->execute()) {
+    $datosViernes = $horarioViernes->fetchAll();
+}else{echo"No hay viernes";}
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -14,43 +49,21 @@ $idProfe = $_GET['profe'];
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../css/horario.css">
+    <link rel="stylesheet" href="../css/verHorario.css">
     <title>Horario Profesor</title>
 </head>
 <body>
-    <?php
-            $mysqli = new mysqli('localhost', 'root', '', 'asesoria');
-
-            if ($mysqli->connect_error) {
-                die('Connect Error (' . $mysqli->connect_errno . ') '
-                        . $mysqli->connect_error);
-            }
-        
-            if (mysqli_connect_error()) {
-                die('Connect Error (' . mysqli_connect_errno() . ') '
-                        . mysqli_connect_error());
-            }
-            $acentos = $mysqli->query("SET NAMES 'utf-8'");
-            $datosq = $mysqli->query("SELECT * FROM profesor WHERE noTrabajador='$idProfe'");
-            $resultado = $datosq->fetch_assoc();
-    ?>
     <div class="banner">
         <img src="../img/escudo_negativo.png" alt="LogoBuap">
-        <h1>Profesor: <?php echo $resultado['nombre']?> </h1>
+        <h1>Profesor: <?php echo $datosP['nombre']?> </h1>
     </div>
     <section>
         <?php
-            $busqueda1=$mysqli->query("SELECT * from lunes where noTrabajador='$idProfe'");
-            $busqueda2=$mysqli->query("SELECT * from martes where noTrabajador='$idProfe'");
-            $busqueda3=$mysqli->query("SELECT * from miercoles where noTrabajador='$idProfe'");
-            $busqueda4=$mysqli->query("SELECT * from jueves where noTrabajador='$idProfe'");
-            $busqueda5=$mysqli->query("SELECT * from viernes where noTrabajador='$idProfe'");
-        
-            while($datos1=$busqueda1->fetch_array()){
-                while($datos2=$busqueda2->fetch_array()){
-                    while($datos3=$busqueda3->fetch_array()){
-                        while($datos4=$busqueda4->fetch_array()){
-                            while($datos5=$busqueda5->fetch_array()){
+            foreach($datosLunes as $lunes){
+                foreach($datosMartes as $martes){
+                    foreach($datosMiercoles as $miercoles){
+                        foreach($datosJueves as $jueves){
+                            foreach($datosViernes as $viernes){
             ?>
         <table id="tablaH" border="1px" align="center" class="table table-sm">
         <thead>
@@ -65,99 +78,99 @@ $idProfe = $_GET['profe'];
         </thead>
         <tr>
             <th>7:00-8:00</th>
-            <td><?php echo $datos1["siete"]?></td>
-            <td><?php echo $datos2["siete"]?></td>
-            <td><?php echo $datos3["siete"]?></td>
-            <td><?php echo $datos4["siete"]?></td>
-            <td><?php echo $datos5["siete"]?></td>
+            <td><?php echo $lunes["siete"]?></td>
+            <td><?php echo $martes["siete"]?></td>
+            <td><?php echo $miercoles["siete"]?></td>
+            <td><?php echo $jueves["siete"]?></td>
+            <td><?php echo $viernes["siete"]?></td>
         </tr>
         <tr>
             <th>8:00-9:00</th>
-            <td><?php echo $datos1["ocho"]?></td>
-            <td><?php echo $datos2["ocho"]?></td>
-            <td><?php echo $datos3["ocho"]?></td>
-            <td><?php echo $datos4["ocho"]?></td>
-            <td><?php echo $datos5["ocho"]?></td>
+            <td><?php echo $lunes["ocho"]?></td>
+            <td><?php echo $martes["ocho"]?></td>
+            <td><?php echo $miercoles["ocho"]?></td>
+            <td><?php echo $jueves["ocho"]?></td>
+            <td><?php echo $viernes["ocho"]?></td>
         </tr>
         <tr>
             <th>9:00-10:00</th>
-            <td><?php echo $datos1["nueve"]?></td>
-            <td><?php echo $datos2["nueve"]?></td>
-            <td><?php echo $datos3["nueve"]?></td>
-            <td><?php echo $datos4["nueve"]?></td>
-            <td><?php echo $datos5["nueve"]?></td>
+            <td><?php echo $lunes["nueve"]?></td>
+            <td><?php echo $martes["nueve"]?></td>
+            <td><?php echo $miercoles["nueve"]?></td>
+            <td><?php echo $jueves["nueve"]?></td>
+            <td><?php echo $viernes["nueve"]?></td>
         </tr>
         <tr>
             <th>10:00-11:00</th>
-            <td><?php echo $datos1["diez"]?></td>
-            <td><?php echo $datos2["diez"]?></td>
-            <td><?php echo $datos3["diez"]?></td>
-            <td><?php echo $datos4["diez"]?></td>
-            <td><?php echo $datos5["diez"]?></td>
+            <td><?php echo $lunes["diez"]?></td>
+            <td><?php echo $martes["diez"]?></td>
+            <td><?php echo $miercoles["diez"]?></td>
+            <td><?php echo $jueves["diez"]?></td>
+            <td><?php echo $viernes["diez"]?></td>
         </tr>
         <tr>
             <th>11:00-12:00</th>
-            <td><?php echo $datos1["once"]?></td>
-            <td><?php echo $datos2["once"]?></td>
-            <td><?php echo $datos3["once"]?></td>
-            <td><?php echo $datos4["once"]?></td>
-            <td><?php echo $datos5["once"]?></td>
+            <td><?php echo $lunes["once"]?></td>
+            <td><?php echo $martes["once"]?></td>
+            <td><?php echo $miercoles["once"]?></td>
+            <td><?php echo $jueves["once"]?></td>
+            <td><?php echo $viernes["once"]?></td>
         </tr>
         <tr>
             <th>12:00-13:00</th>
-            <td><?php echo $datos1["doce"]?></td>
-            <td><?php echo $datos2["doce"]?></td>
-            <td><?php echo $datos3["doce"]?></td>
-            <td><?php echo $datos4["doce"]?></td>
-            <td><?php echo $datos5["doce"]?></td>
+            <td><?php echo $lunes["doce"]?></td>
+            <td><?php echo $martes["doce"]?></td>
+            <td><?php echo $miercoles["doce"]?></td>
+            <td><?php echo $jueves["doce"]?></td>
+            <td><?php echo $viernes["doce"]?></td>
         </tr>
         <tr>
             <th>13:00-14:00</th>
-            <td><?php echo $datos1["trece"]?></td>
-            <td><?php echo $datos2["trece"]?></td>
-            <td><?php echo $datos3["trece"]?></td>
-            <td><?php echo $datos4["trece"]?></td>
-            <td><?php echo $datos5["trece"]?></td>
+            <td><?php echo $lunes["trece"]?></td>
+            <td><?php echo $martes["trece"]?></td>
+            <td><?php echo $miercoles["trece"]?></td>
+            <td><?php echo $jueves["trece"]?></td>
+            <td><?php echo $viernes["trece"]?></td>
         </tr>
         <tr>
             <th>14:00-15:00</th>
-            <td><?php echo $datos1["catorce"]?></td>
-            <td><?php echo $datos2["catorce"]?></td>
-            <td><?php echo $datos3["catorce"]?></td>
-            <td><?php echo $datos4["catorce"]?></td>
-            <td><?php echo $datos5["catorce"]?></td>
+            <td><?php echo $lunes["catorce"]?></td>
+            <td><?php echo $martes["catorce"]?></td>
+            <td><?php echo $miercoles["catorce"]?></td>
+            <td><?php echo $jueves["catorce"]?></td>
+            <td><?php echo $viernes["catorce"]?></td>
         </tr>
         <tr>
             <th>15:00-16:00</th>
-            <td><?php echo $datos1["quince"]?></td>
-            <td><?php echo $datos2["quince"]?></td>
-            <td><?php echo $datos3["quince"]?></td>
-            <td><?php echo $datos4["quince"]?></td>
-            <td><?php echo $datos5["quince"]?></td>
+            <td><?php echo $lunes["quince"]?></td>
+            <td><?php echo $martes["quince"]?></td>
+            <td><?php echo $miercoles["quince"]?></td>
+            <td><?php echo $jueves["quince"]?></td>
+            <td><?php echo $viernes["quince"]?></td>
 
         <tr>
             <th>16:00-17:00</th>
-            <td><?php echo $datos1["diezseis"]?></td>
-            <td><?php echo $datos2["diezseis"]?></td>
-            <td><?php echo $datos3["diezseis"]?></td>
-            <td><?php echo $datos4["diezseis"]?></td>
-            <td><?php echo $datos5["diezseis"]?></td>
+            <td><?php echo $lunes["diezseis"]?></td>
+            <td><?php echo $martes["diezseis"]?></td>
+            <td><?php echo $miercoles["diezseis"]?></td>
+            <td><?php echo $jueves["diezseis"]?></td>
+            <td><?php echo $viernes["diezseis"]?></td>
         </tr>
         <tr>
             <th>17:00-18:00</th>
-            <td><?php echo $datos1["diezsiete"]?></td>
-            <td><?php echo $datos2["diezsiete"]?></td>
-            <td><?php echo $datos3["diezsiete"]?></td>
-            <td><?php echo $datos4["diezsiete"]?></td>
-            <td><?php echo $datos5["diezsiete"]?></td>
+            <td><?php echo $lunes["diezsiete"]?></td>
+            <td><?php echo $martes["diezsiete"]?></td>
+            <td><?php echo $miercoles["diezsiete"]?></td>
+            <td><?php echo $jueves["diezsiete"]?></td>
+            <td><?php echo $viernes["diezsiete"]?></td>
         </tr>
         <tr>
             <th>18:00-19:00</th>
-            <td><?php echo $datos1["diezocho"]?></td>
-            <td><?php echo $datos2["diezocho"]?></td>
-            <td><?php echo $datos3["diezocho"]?></td>
-            <td><?php echo $datos4["diezocho"]?></td>
-            <td><?php echo $datos5["diezocho"]?></td>
+            <td><?php echo $lunes["diezocho"]?></td>
+            <td><?php echo $martes["diezocho"]?></td>
+            <td><?php echo $miercoles["diezocho"]?></td>
+            <td><?php echo $jueves["diezocho"]?></td>
+            <td><?php echo $viernes["diezocho"]?></td>
         </tr>
     <?php 
                     }
@@ -168,7 +181,7 @@ $idProfe = $_GET['profe'];
     ?>
     </table>
     <a href="../busqueda.php" id="liga" class="stretch-link text-primary">Volver al buscador</a>
-    <a href="../agendarAsesoria.php" id="asesoria" class="stretch-link text-primary"></a>
+    <a href="../agendarAsesoria.php" id="asesoria" class="stretch-link text-primary">Agendar Asesoria</a>
     </section>
 </body>
 </html>
